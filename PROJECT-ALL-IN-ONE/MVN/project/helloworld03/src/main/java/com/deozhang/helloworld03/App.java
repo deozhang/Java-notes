65c672d2ba21ed5cb2fc628bf9a70c18e84dfab3
nihaoprogrammer <46418059+nihaoprogrammer@users.noreply.github.com>
2019-07-22 02:05:55 +0800

    增加关于maven的项目,由于maven仓库没有正确的权限,试了3次成功创建.

---
+++ PROJECT-ALL-IN-ONE/MVN/project/helloworld03/src/main/java/com/deozhang/helloworld03/App.java
@@ -0,0 +1,13 @@
+package com.deozhang.helloworld03;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
